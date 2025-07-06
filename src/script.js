2fbb745bafc0817c94848d76920367c3e76c16ff
elias.alves <elias.alves@azi.com.br>
2025-07-06 18:59:15 -0400

    adding comments to adjust

--- src/script.js
+++ src/script.js
@@ -5,17 +5,19 @@ function createCard(title, username, content, likes) {
     const titleCard = document.createElement("h2")
     const usernameCard = document.createElement("p")
     const contentCard = document.createElement("p")
-    const buttonLike = document.createElement("button")
+    const buttonLike = document.createElement("button") // TODO remover constante nao utilizada
     const like = document.createElement("span")
     const divButtonLikeComment = document.createElement("div")
     const icon = document.createElement("ion-icon")
     const divLike = document.createElement("div")
 
-    clickHeart(icon)
+    clickHeart(icon) // chamar funcao no fim de tudo
 
+    
     divCard.classList.add("cards")
-    divButtonLikeComment.classList.add("btn-like-comment")
-
+    divButtonLikeComment.classList.add("btn-like-comment") 
+    
+    // aqui ja está sendo utilizado o padrao classList.add(). continuar seguindo o padrão ao inves de usar o className
     divLike.className = 'large-font text-center top-20'
 
     icon.setAttribute("name", "heart")
@@ -25,6 +27,10 @@ function createCard(title, username, content, likes) {
     contentCard.innerText = content
     like.innerText = likes
 
+    // aqui é possível criar uma função para adicionar filhos a uma tag
+    // sempre que ver muito código se repetindo(appendChild()), estranhe, pense que isso poderia ser uma funcao reutilizavel
+    // const adicionarFilho = (tagPai, tagFilha) => tagPai.appendChild(tagFilha)
+    // adicionarFilho(divCard,titleCard)
     divCard.appendChild(titleCard)
     divCard.appendChild(usernameCard)
     divCard.appendChild(contentCard)
