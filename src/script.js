b8fea0bf28ac3e40c9a2318fc9ea2cddda434124
Desktop Ruan <ruanhbraga@gmail.com>
2025-07-05 22:31:04 -0300

    ajustes code review

--- src/script.js
+++ src/script.js
@@ -2,11 +2,12 @@ function createCard (title, username, content){
 
     const cards = document.querySelector(".container_cards")
     const divCard = document.createElement("div")
-    divCard.classList.add("cards")
     const titleCard = document.createElement("h2")
     const usernameCard = document.createElement("p")
     const contentCard = document.createElement("p")
-   
+    
+    divCard.classList.add("cards")
+    
     titleCard.innerText = title
     usernameCard.innerText = username
     contentCard.innerText = content
@@ -16,9 +17,6 @@ function createCard (title, username, content){
     divCard.appendChild(contentCard)
     cards.appendChild(divCard)
 }
-
-
-createCard("titulo", "nome de usuario", "conteudo")
 
 async function buscar() {
     const response = await fetch(`https://api-posts.thesigns.com.br/posts`)
